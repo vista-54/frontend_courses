d9d2096e2d11745fa8e39434c681d00574280de1
Vista <vista-54@ukr.net>
2017-09-21 19:28:12 +0300

    init

---
+++ app/app.module.js
@@ -0,0 +1,18 @@
+;(function () {
+    'use strict';
+
+    angular.module('myApp', [
+        'ui.router'
+    ])
+
+
+
+        // .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+        //     $locationProvider.hashPrefix('!');
+        //
+        //     // $routeProvider.otherwise({redirectTo: '/view1'});
+        // }]);
+})();
+
+
+
